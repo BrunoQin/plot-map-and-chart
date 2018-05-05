acc1eb76e30e9574174e3a5da1b5d11a616d2c6a
BrunoQin <brunoqin@163.com>
2018-05-05 12:34:12 +0800

    修改cmap

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Plot Map and Chart
+
+```
+brew install netcdf --with-cxx-compat % mac安装netcdf
+pip install Cython
+pip install netcdf4
+pip install pillow
+pip install geos
+conda install matplotlib
+pip install pyproj
+conda install -c conda-forge basemap-data-hires=1.0.8.dev0
+```
--- Read_Plot.py
+++ Read_Plot.py
@@ -1,22 +1,25 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
+from matplotlib.colors import LinearSegmentedColormap
 import numpy as np
 import netCDF4
 
-file = netCDF4.Dataset('/Users/Bruno/Desktop/ocean_temp_salt_0.nc')
-
+file = netCDF4.Dataset('/Users/Bruno/Desktop/ocean_temp_salt_6.nc')
 lat = file.variables['yaxis_1'][:]
-
 lon = file.variables['xaxis_1'][:]
-
 data = file.variables['temp'][0, 0, :, :]
 file.close()
 
-m = Basemap(llcrnrlon=-279.5, urcrnrlon=79.5, llcrnrlat=-81.5, urcrnrlat=89.5, resolution='c')
+data[data == 0] = 'nan'
 
-m.drawcoastlines(linewidth=0.1)
-m.fillcontinents(color='coral', lake_color='aqua')
+# 自定义画图的颜色
+# cmap1 = LinearSegmentedColormap.from_list("my_colormap", ((170/255., 213/255., 227/255.), (176/255., 219/255., 233/255.)), N=6, gamma=1.0)
+
+m = Basemap(projection="cyl", llcrnrlon=-279.5, urcrnrlon=79.5, llcrnrlat=-81.5, urcrnrlat=89.5, resolution='c')
+
 m.drawmapboundary(fill_color='aqua')
+m.fillcontinents(color='coral',lake_color='aqua')
+m.drawcoastlines(linewidth=0.1)
 
 m.drawparallels(np.arange(-90, 90, 30), labels=[1, 0, 0, 0], linewidth=0.3, color='k', fontsize=10)
 m.drawmeridians(np.arange(-180, 180, 60), labels=[0, 0, 0, 1], linewidth=0.1, color='k', fontsize=10)
